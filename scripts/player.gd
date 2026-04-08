7e9c9e0d051ec1638171818cb116271cda78032a
NullBot-hd <211388817+NullBot-hd@users.noreply.github.com>
2026-04-08 15:10:44 +0200

    Test Game

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
 
+@onready var animatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +12,31 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("moveLeft", "moveRight")
+	
+	# Flips the player into the right direction
+	if direction < 0:
+		animatedSprite2D.flip_h = true
+	elif direction > 0:
+		animatedSprite2D.flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# Play animation	
+	if is_on_floor():
+		if direction == 0:
+			animatedSprite2D.play("idle")
+		else:
+			animatedSprite2D.play("run")
+	else:
+		animatedSprite2D.play("jump")	
+		
 	move_and_slide()
