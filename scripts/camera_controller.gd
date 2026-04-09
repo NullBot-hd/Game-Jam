38c28a15b98bab9dbbdb8b4e54863c9214b95123
NullBot-hd <211388817+NullBot-hd@users.noreply.github.com>
2026-04-09 15:23:23 +0200

    Added Custom Camera Scaling

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -3,12 +3,42 @@ extends Node2D
 @export var player_0: CharacterBody2D
 @export var player_1: CharacterBody2D
 
+@onready var camera: Camera2D = $Camera2D
+
+@export var margin: float = 1 
+@export var min_zoom: float = 3
+@export var max_zoom: float = 4
+@export var zoom_speed: float = 5.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(str(calcOffest()))
-	$".".position = player_1.position + calcOffest()
+	#print(str(get_viewport_transform()))
+	#print(str(calcOffset().x))
+	#camera.position = player_1.position + .5 * calcOffset()
+	
+	if not player_0 or not player_1:
+		return
+		
+	camera.position = (player_0.position + player_1.position) / 2.0
+	
+	var distance = calcOffset()
+	var distance_x = abs(distance.x)
+	var distance_y = abs(distance.y)
+	
+	var screen_size = get_viewport_rect().size
+	var target_zoom_x = (screen_size.x) / (distance_x + margin) * .5
+	
+	print("X" + str(screen_size.x / 8) + "/" + str(distance_x) + "+" + str(margin))
+		
+	#print("X" + str(target_zoom_x))
+	
+	print(target_zoom_x)
+	target_zoom_x = clamp(target_zoom_x, min_zoom, max_zoom)
+	var target_zoom = Vector2(target_zoom_x, target_zoom_x)
+	print(target_zoom)
+	camera.zoom = camera.zoom.lerp(target_zoom, zoom_speed * delta)
 
-func calcOffest() -> Vector2:
-	var vector = .5 * (player_0.position - player_1.position)
+
+func calcOffset() -> Vector2:
+	var vector = (player_0.position - player_1.position)
 	return vector
