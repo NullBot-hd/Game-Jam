21c173429422b93bde9d0a9a563d30ed7b2462ca
NullBot-hd <211388817+NullBot-hd@users.noreply.github.com>
2026-04-09 12:52:13 +0200

    Implemented Character Animation

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -4,17 +4,11 @@ extends Node2D
 @export var player_1: CharacterBody2D
 
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	print(str(calcOffest()))
-	$".".position = calcOffest()
+	$".".position = player_1.position + calcOffest()
 
 func calcOffest() -> Vector2:
-	var vector = player_1.position + .5 * (player_0.position - player_1.position)
+	var vector = .5 * (player_0.position - player_1.position)
 	return vector
