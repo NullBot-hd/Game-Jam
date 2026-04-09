1bd8dfd933c629618f57f420526b301dfdd066bc
NullBot-hd <211388817+NullBot-hd@users.noreply.github.com>
2026-04-09 19:36:34 +0200

    Removed Coins & Removed Platforms & Minor bug fix

--- scripts/coin.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@onready var gameManager: Node = %GameManager
-@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
- 
-func _on_body_entered(body: Node2D) -> void:		
-	gameManager.add_point()
-	animationPlayer.play("pickup")
